30e62c272db8b3160bc643be04ee461e2d01d162
severn-everett <severn.everett@gmail.com>
2017-10-24 11:06:10 +0200

    Optimized start-up of program by removing unnecessary auto-config loading

--- src/main/kotlin/com/severett/kotlin_ras/MainApp.kt
+++ src/main/kotlin/com/severett/kotlin_ras/MainApp.kt
@@ -1,11 +1,40 @@
 package com.severett.kotlin_ras
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration
+import org.springframework.boot.autoconfigure.web.HttpEncodingAutoConfiguration
+import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration
+import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration
+import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 
-@SpringBootApplication
-open class MainApp {
-}
+@ComponentScan
+@Configuration
+@Import(
+        DispatcherServletAutoConfiguration::class,
+        EmbeddedServletContainerAutoConfiguration::class,
+        ErrorMvcAutoConfiguration::class,
+        HttpEncodingAutoConfiguration::class,
+        HttpMessageConvertersAutoConfiguration::class,
+        JacksonAutoConfiguration::class,
+        PropertyPlaceholderAutoConfiguration::class,
+        ServerPropertiesAutoConfiguration::class,
+        ValidationAutoConfiguration::class,
+        WebMvcAutoConfiguration::class
+)
+open class MainApp
 
 fun main(args : Array<String>) {
     SpringApplication.run(MainApp::class.java, *args)
